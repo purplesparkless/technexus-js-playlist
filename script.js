1505b7bfb287f391bc9a3a713d59aa3dc47e8489
Zainab Sansa <tunrayotemitope05@gmail.com>
2024-02-01 22:06:31 +0100

    new commit

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 // Official name of javascript is ECMAScript. It has nothing to do with JAVA
 // JS is case-sentitive. Runs from top to bottom. Not sensitive to white breaks
 alert("Hello World")
+
+// VARIABLES
+var myVariable = "Hello";
